e68a0c5b56fc271ae9b047fe0db146719fc6de1a
Milos Peric <milosperic065@gmail.com>
2026-04-20 20:44:11 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b4d03777-8f56-475c-bf4d-6acecff52035';
+export const MODAL_CLOSE_SEC = 2.5;
